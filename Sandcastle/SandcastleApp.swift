3522b5af7bdde8b52fd09a834f96942214295025
Leptos <leptos.0.null@gmail.com>
2026-02-19 06:20:05 -0800

    Add blank iOS app Xcode project

---
+++ Sandcastle/SandcastleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandcastleApp.swift
+//  Sandcastle
+//
+//  Created by Leptos on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct SandcastleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
